55d056c1ff9722558e99ba6b7b1d06e9c4ed3a56
Unknown <ruiztorresmatias@gmail.com>
2018-09-10 17:35:00 -0300

    E-0.0.6
    Material files work in progress

--- Engine/EngineDLL/Material.h
+++ Engine/EngineDLL/Material.h
@@ -5,5 +5,7 @@ class ENGINEDLL_API Material
 public:
 	Material();
 	~Material();
+	void Bind(unsigned int programId);
+	unsigned int LoadShaders(const char * vertex_file_path, const char * fragment_file_path);
 };
 
